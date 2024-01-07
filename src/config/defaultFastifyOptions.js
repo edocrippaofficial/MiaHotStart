a5fd248bc734f23ad427d97a54824ee278206409
edoardocrippaofficial <edoardocrippa96@gmail.com>
2024-01-07 18:46:45 +0100

    Default options comments

--- src/config/defaultFastifyOptions.js
+++ src/config/defaultFastifyOptions.js
@@ -23,17 +23,20 @@ const logger = {
   },
 }
 
+// 2,147,483,647 (2^31 − 1) stands for max SMI value (an internal optimization of V8).
+const virtualMaxInt = 2147483647
+
 // Very opinionated default options for the Fastify server instance
 module.exports = {
   logger,
+  // even after closing the server, it routes the incoming request as usual
   return503OnClosing: false,
-  ignoreTrailingSlash: false,
-  caseSensitive: true,
   // use “legacy” header version with prefixed x- for better compatibility with existing enterprises infrastructures
   requestIdHeader: 'x-request-id',
-  // set 30 seconds to
+  // set 30 seconds to plugins to load
   pluginTimeout: 30000,
   // virtually disable the max body size limit
-  bodyLimit: Number.MAX_SAFE_INTEGER,
+  bodyLimit: virtualMaxInt,
+  // do not log requests and replies since we have a custom logger
   disableRequestLogging: true,
 }
