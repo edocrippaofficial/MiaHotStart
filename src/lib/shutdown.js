dba62524c7e6651a72a345a130bf10126de36d4e
edoardocrippaofficial <edoardocrippa96@gmail.com>
2024-01-08 08:56:41 +0100

    Refactoring shutdown

--- src/lib/shutdown.js
+++ src/lib/shutdown.js
@@ -1,26 +1,26 @@
 'use strict'
 
-// eslint-disable-next-line no-shadow
-const { setTimeout } = require('node:timers/promises')
+const { setTimeout: sleep } = require('node:timers/promises')
 
 module.exports = async function shutdown(fastify, opts) {
   if (opts.disableGracefulShutdown) {
     return
   }
 
-  fastify.addHook('onClose', () => {
-    fastify.log.fatal('Server closed after graceful shutdown')
-  })
-  process.on('SIGTERM', () => {
+  process.on('SIGTERM', async() => {
     // Google Kubernetes Engine (GKE) is 10 sec,
     // so the worst case, the iptables rule will
     // be updated 10 seconds later after the pod
     // deletion event arrives
     // https://blog.laputa.io/graceful-shutdown-in-kubernetes-85f1c8d586da
     fastify.log.fatal('SIGTERM signal received.')
-    setTimeout(opts.gracefulShutdownSeconds * 1000)
-      .then(() => {
-        fastify.close()
-      })
+
+    await sleep(opts.gracefulShutdownSeconds * 1000)
+
+    fastify.log.fatal('Starting server close after graceful shutdown')
+    await fastify.close()
+
+    // eslint-disable-next-line no-console
+    console.log('Server closed after graceful shutdown')
   })
 }
